927ad84ae90ad75a9f33fbe752b1cffef135ff28
Gopolang <mogashoag@yahoo.com>
2023-10-16 10:19:03 -0700

    commit

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import './AboutPage.css'
 
-const AboutPage = () => {
+function YourComponent() {
   return (
-    <div>
-      <h1>About Sweet Harmony Bakery</h1>
-      <p>Background: Founded in 2019 by the visionary baker Gopolang, Sweet Harmony Bakery is a culinary haven nestled in our community. Gopolang's passion for baking and her dedication to crafting delectable pastries culminated in the birth of this cozy bakery. Sweet Harmony quickly became a local favorite, renowned for its exquisite flavors and warm, welcoming ambiance.
-
-Mission: At Sweet Harmony Bakery, our mission is simple yet profound: to create joy through the art of baking. We infuse love and care into every delectable creation, offering a symphony of flavors that harmonize with our customers' most cherished moments. Our commitment to quality, innovation, and community makes us more than just a bakery; we're a source of happiness and delight for all who walk through our doors.
-</p>
-    
+    <div className="container col-xxl-8 px-4 py-5">
+      <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+        <div className="col-10 col-sm-8 col-lg-6">
+          <img src="bootstrap-themes.png" className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
+        </div>
+        <div className="col-lg-6">
+          <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">About Sweet Harmony</h1>
+          <p className="lead">Founded by Gopolang, a passionate baker with a rich culinary background, our bakery is a haven for artisanal treats. With a deep-seated love for the art of baking, Gopolang brings years of experience to every creation. Located in the heart of [Your Location], our bakery is a warm, welcoming space where the heavenly aroma of freshly baked goods greets you at the door. From buttery croissants to intricately decorated cakes, each item is a testament to our commitment to quality and flavor. We believe in making every moment special with our delectable offerings. Come savor the taste of passion and tradition at Sweet Harmony.</p>
+          <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+            <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Learm more</button>
+            <button type="button" className="btn btn-outline-secondary btn-lg px-4">Default</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default AboutPage;
+
 

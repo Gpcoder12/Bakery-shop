73de511810c124f82541db2826dd9807bb8b7632
Gopolang <mogashoag@yahoo.com>
2023-10-16 05:08:51 -0700

    commit

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import './HomePage.css';
-
 
 const HomePage = () => {
+  const containerStyle = {
+    maxWidth: '800px',      // Container dimensions
+    margin: '0 auto',       // Center horizontally
+    padding: '20px',        // Padding
+    backgroundColor: '#f7f7f7',  // Background color
+    backgroundImage: 'url("your-background-image-url.jpg")',  // Background image
+    backgroundSize: 'cover',  // Background image size
+  };
+
+  const h1Style = {
+    color: 'teal',          // Text color
+    fontSize: '28px',       // Font size
+    fontStyle: 'italic',   // Font style
+    textAlign: 'center',    // Text alignment
+  };
+
+  const pStyle = {
+    textAlign: 'center',    // Text alignment
+  };
+
   return (
-    <div className="container">
-      <h1>Welcome to Sweet Harmony Bakery</h1>
-      <p>Crafting Sweet Memories,One Bite at a time</p>
-      
+    <div style={containerStyle}>
+      <h1 style={h1Style}>Welcome to Sweet Harmony Bakery</h1>
+      <p style={pStyle}>Crafting Sweet Memories, One Bite at a Time</p>
     </div>
-   
   );
 };
 
 export default HomePage;
+
 

cfd0c0107d5569c30f595ec746c9d21407eccecd
Gopolang <mogashoag@yahoo.com>
2023-10-16 10:44:14 -0700

    commit

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -6,7 +6,7 @@ function AboutPage() {
     <div className="container col-xxl-8 px-4 py-5">
       <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
         <div className="col-10 col-sm-8 col-lg-6">
-          <img src="bootstrap-themes.png" className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
+          <img src="https://media.istockphoto.com/id/693282894/photo/happy-african-baker-man.jpg?s=612x612&w=0&k=20&c=8HyM5vkHwexoOMzKFE4azjbfurPcucLIDFXzLFuwuCE=" className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy" />
         </div>
         <div className="col-lg-6">
           <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">About Sweet Harmony</h1>
